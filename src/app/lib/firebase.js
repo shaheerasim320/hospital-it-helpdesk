656b89ab77dfc19f3d8c81725b5b9373a978ced9
Shaheer <sp23-bcs-063@cuilahore.edu.pk>
2025-07-31 23:01:22 +0500

    Firebase Setup

---
+++ src/app/lib/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVEQgayzqTuRQmYEG7mEQ9D-1pngaJdcI",
+  authDomain: "hospital-it-help-desk.firebaseapp.com",
+  projectId: "hospital-it-help-desk",
+  storageBucket: "hospital-it-help-desk.firebasestorage.app",
+  messagingSenderId: "520614879541",
+  appId: "1:520614879541:web:901e7609a938f7b799ec7c",
+  measurementId: "G-279JQVS0PL"
+};
+
+const app = initializeApp(firebaseConfig);
+
+let analytics;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);

5305e4bfc4c21b29d8429dbd7b94d60817b070d9
Shaheer <sp23-bcs-063@cuilahore.edu.pk>
2025-08-08 01:33:52 +0500

    Fixed upercase issue

--- src/components/AdminRoute.jsx
+++ src/components/AdminRoute.jsx
@@ -3,7 +3,7 @@ import { getUserFromToken } from "@/lib/auth";
 import { redirect } from "next/navigation";
 export default async function AdminRoute({ children }) {
     const user = await getUserFromToken();
-    if (!user || user.role !== 'Admin') {
+    if (!user || user.role !== 'admin') {
         redirect('/login');
     }
     return <>{children}</>
